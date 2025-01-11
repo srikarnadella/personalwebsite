3a8b972089cea891a7a0c170d1ddfa6997b2c7bb
Srikar Nadella <nadellaspam@gmail.com>
2025-01-11 02:09:46 -0500

    Removed Resume dropdown

--- app/components/ResumeDropdown.js
+++ app/components/ResumeDropdown.js
@@ -39,28 +39,12 @@ const ResumeDropdown = () => {
         <div className="absolute left-0 mt-2 w-56 origin-top-left bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none">
           <div className="py-1">
             <a
-              href="https://drive.google.com/file/d/1g_E7de5LmT9adWlSACQExIaHzaWICMJC/view?usp=sharing"
+              href="https://drive.google.com/file/d/1dKQSwVRxtKX64YR0P2MihrdwaE81Wy-z/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
             >
-              Software Engineering Resume
-            </a>
-            <a
-              href="https://drive.google.com/file/d/1AW3BdxNJ7t0aaWvcM6pBGjUYRWyQehIz/view?usp=sharing"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-            >
-              Data Analyics Resume
-            </a>
-            <a
-              href="https://drive.google.com/file/d/1j3hkcznciutSwXYvjvgHyAReg9eZ3968/view?usp=sharing"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-            >
-              Finance Resume
+              Resume
             </a>
           </div>
         </div>
